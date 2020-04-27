1dadc7fea8abfd8dbd119c955eae636e4fc66076
ryan <17805315@student.westernsydney.edu.au>
2020-04-27 12:47:18 +1000

    Just Syncing new bbl file

---
+++ report/.auctex-auto/report.el
@@ -0,0 +1,60 @@
+(TeX-add-style-hook
+ "report"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem") ("biblatex" "citestyle=numeric" "bibstyle=numeric" "hyperref=true" "backref=true" "maxcitenames=3" "url=true" "backend=biber" "natbib=true")))
+   (add-to-list 'LaTeX-verbatim-environments-local "minted")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref"
+    "minted"
+    "/home/ryan/Dropbox/profiles/Templates/LaTeX/ScreenStyle"
+    "biblatex")
+   (LaTeX-add-labels
+    "sec:org6d64684"
+    "sec:orga2e7a3d"
+    "orgc6aad20"
+    "org113e1ba"
+    "orgf23ed37"
+    "sec:org2cbea10"
+    "orgd40993d"
+    "sec:orge45d8dc"
+    "org088138d"
+    "sec:org0d26979"
+    "orgc71104e"
+    "sec:org1632e35"
+    "sec:orgec90a05"
+    "org0c3072e"
+    "fig:org1bcabb4"
+    "sec:org7374ef1"
+    "org3f0983d"
+    "orgb8f0c16")
+   (LaTeX-add-bibliographies
+    "/home/ryan/Dropbox/Studies/Papers/references"
+    "references"))
+ :latex)
+
