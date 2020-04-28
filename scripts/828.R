d5961bc683f54ae3f05dd2b5fedc9ae01dbddeca
ryan <17805315@student.westernsydney.edu.au>
2020-04-28 11:02:39 +1000

    Created Script for Question 8.2.8

---
+++ scripts/828.R
@@ -0,0 +1,31 @@
+# * Load Packages -----------------------------------------------------------
+setwd("~/Dropbox/Notes/DataSci/Social_Web_Analytics/SWA-Project/scripts/")
+
+if (require("pacman")) {
+  library(pacman)
+} else{
+  install.packages("pacman")
+  library(pacman)
+}
+
+pacman::p_load(xts, sp, gstat, ggplot2, rmarkdown, reshape2, ggmap, parallel,
+               dplyr, plotly, tidyverse, reticulate, UsingR, Rmpfr, swirl,
+               corrplot, gridExtra, mise, latex2exp, tree, rpart, lattice, coin,
+               primes, epitools, maps, clipr, ggmap, twitteR, ROAuth, tm,
+               rtweet, base64enc, httpuv, SnowballC, RColorBrewer, wordcloud,
+               ggwordcloud, boot)
+
+mise()
+
+# ** Load the Tweets ==============================================================
+mise(); load("./resources/Download_1.Rdata")
+
+# * Heading 1 ------------------------------------------------------------------
+#' Feel free to use Roxygen comments
+# ** Heading 2 =================================================================
+# *** Heading 3 ################################################################
+# **** Heading 4
+# ***** Heading 5
+# ****** Heading 6
+## Comments have two `#`
+                                        # right aligned have one `#`
