RyanGreenup/SWA-Project

commit a105a8f3560377afe82c22b230d2d9fea25f1a8f

[8.2.17] Added Cluster Count to Report

scripts/811.R
@@ -612,5 +612,21 @@ ggplot(pca_data, aes(x = MDS1, y = MDS2, col = Friend_Status)) +
   scale_color_discrete(label = c("High Friends", "Low Friends")) +
   guides(col = guide_legend("Friend Count \n Status"))
 
+## *** Inspect the Friend Counts (Continuous Data)################################
+#' What I could do is visualise the number of friends as colour and size with
+#the cluster ellipses as well, I have removed data by indicating that above
+#average is above average regardless of amount.
 
-# do it all again to check ------------------------------------------------
+## * 8.2.17 Which Cluster has the highest above-average friend counts ?---------   :8217:
+(clust_friend <- table(
+  pca_data[,names(pca_data) %in% c("Cluster", "Friend_Status")])
+)
+
+cbind(clust_friend,
+        "Proportion" = signif(
+            (clust_friend[,1] / rowSums(clust_friend)),
+            2)
+      )
+
+
+names(pca_data)

report/report.org
@@ -27,6 +27,17 @@
 #+latex_header: %%% TeX-command-extra-options: "-shell-escape"
 :END:
 
+
+#+begin_comment
+TODO Does it make sense to plot the average friend user like in [[pt2]]
+Really we need to
+  + take into consideration that a user with 9999 friends is better than a user with 666 friends even if the average is 500
+  + 0.4 is a lot better than 0.2, so why doesn't my chart reflect that?
+
+It might be more appropriate to take this plot out, it might only confuse things
+#+end_comment
+
+
 * 8.1 Analysing the Relationship Between Friends and Followers for Twitter Users
 ** 8.1.1 Retrieve the posts from Twitter
 relevant posts can be retrieved from twitter by utilising the =rtweet= package, packages can be loaded for use in **_R_** thusly:
@@ -907,6 +918,7 @@ colour is demonstrated in listing [[pt1c]] shown in [[pt2]], In order to get a b
 understanding of the distribution of friend counts a plot of the friend count
 status mapped to colour is demonstrated in listing [[pt2c]] and shown in figure [[pt2]].
 
+
 #+NAME: cmds
 #+CAPTION: Use /Multi-Dimensional/ scaling to project the data into 2 dimensions
 #+begin_src R
@@ -961,7 +973,34 @@ ggplot(pca_data, aes(x = MDS1, y = MDS2, col = Cluster)) +
 [[./Figures/betfriendplot8216.png]]
 
 ** 8.2.17 Comment on the Visualisation
-Figure [[pt1]] indicates that altough there isn't a clear distinction between clusters there is seperation of the tweets in such a way that does allow some degree of classification to occur. Figure [[pt1]] and [[pt2]] both indicate that having many or few friends is reasonably independent of the cluster that the tweet belongs to.
+Figure [[pt1]] indicates that altough there isn't a clear distinction between
+clusters there is seperation of the tweets in such a way that does allow some
+degree of classification to occur. Figure [[pt1]] and [[pt2]] both indicate that having
+many or few friends is reasonably independent of the cluster that the tweet
+belongs to.
+** 8.2.18 Cluster with Highest Number Friends
+The Number of above or below friends corresponsing to a given cluster can be tabulated by using the =table= function as shown in listing [[tfc]] and table [[tfct]], this indicates that there is a difference in the proportion of users with above average friend counts between clusters with cluster =#1= having the highest proportion of users with above average friend counts.
+
+#+NAME: tfc
+#+CAPTION: Tabulate the distribution of friends in
+#+begin_src R
+(clust_friend <- table(
+  pca_data[,names(pca_data) %in% c("Cluster", "Friend_Status")])
+)
+
+cbind(clust_friend,
+        "Proportion" = signif(
+            (clust_friend[,1] / rowSums(clust_friend)),
+            2)
+      )
+#+end_src
+
+#+NAME: tfct
+#+CAPTION: Above Average Friend Counts Across Clusters
+| */Cluster/* | */Above Average/* | */Below Average/* | */Proportion/* |
+| =1=         |                17 |                27 |           0.39 |
+| =2=         |               141 |               407 |           0.26 |
+| =3=         |                16 |                64 |           0.20 |
 
 * Appendix
 ** Users with High Friend Count

commit 3ea2bb9a8d7663e39b85917560a17cbd595c5cdf

I fixed the varname error (high_friend -> highds) caused by a105a8f

scripts/811.R
@@ -268,45 +268,44 @@ s <- replicate(10^4,{
 
 mean(s)
 
-## * 8.2.8 Find users with Above Average Friend Count-------------------------     :816:
-## <>>
-## yiends count, see [[8.1.3]]
-## Rr these users must not be duplicated
+## * FIXME 8.2.8 Find users with Above Average Friend Count-------------------------     :816:
+## <<8.2.8>>
+## y is friends count, see [[8.1.3]]
+## Remember these users must not be duplicated
 
-## <812>>
-seledplyr::select
-filtdplyr::filter
-inte_vars <- c("user_id", "friends_count")
-(friunts <- tweets.company %>%
-  senterested_vars) %>%
-  fiduplicated(user_id)))
+## <<dplyr812>>
+select <- dplyr::select
+filter <- dplyr::filter
+interested_vars <- c("user_id", "friends_count")
+(friend_counts <- tweets.company %>%
+  select(interested_vars) %>%
+  filter(!duplicated(user_id)))
 
-(hignds <- friend_counts %>%
-  firiends_count > mean(friends_count, na.rm = TRUE)))
+(high_friends <- friend_counts %>%
+  filter(friends_count > mean(friends_count, na.rm = TRUE)))
 
-highds <- high_friends[order(
-  hiends$friends_count,
-  deng = TRUE),]
+high_friends <- high_friends[order(
+  high_friends$friends_count,
+  decreasing = TRUE),]
 
-headfriends)
-tailfriends)
+head(high_friends)
+tail(high_friends)
 
-## * 8.2.8 Find users with Below Average Friend Count-------------------------     :817:
-(lowds <- friend_counts %>%
-  firiends_count <= mean(friends_count, na.rm = TRUE)))
+## * FIXME 8.2.8 Find users with Below Average Friend Count-------------------------     :817:
+(low_friends <- friend_counts %>%
+  filter(friends_count <= mean(friends_count, na.rm = TRUE)))
 
- lowds <- low_friends[order(
-   lends$friends_count,
-   ding = TRUE),]
+ low_friends <- low_friends[order(
+   low_friends$friends_count,
+   decreasing = TRUE),]
 
-headriends)
-tailriends)
+head(low_friends)
+tail(low_friends)
 
 
-if (low_friends) + nrow(high_friends))!=length(users)) {
-  prore users identified that exist, review the method to count high_friends")
+if ((nrow(low_friends) + nrow(high_friends))!=length(users)) {
+  print("More users identified that exist, review the method to count high_friends")
 }
-
 ## * 8.2.10 Find the tweets of those users indentified above------------------     :828:
 ## The point of this is that the data is now ordered, the
 ## top part is the high friends and the low part is the low friends
@@ -617,7 +616,8 @@ ggplot(pca_data, aes(x = MDS1, y = MDS2, col = Friend_Status)) +
 #the cluster ellipses as well, I have removed data by indicating that above
 #average is above average regardless of amount.
 
-## * 8.2.17 Which Cluster has the highest above-average friend counts ?---------   :8217:
+## * 8.2.17 REPORT Comment on Viz -----------------------------------------------  :8217:
+## * 8.2.18 Which Cluster has the highest above-average friend counts ?---------   :8218:
 (clust_friend <- table(
   pca_data[,names(pca_data) %in% c("Cluster", "Friend_Status")])
 )
@@ -631,7 +631,7 @@ cbind(clust_friend,
 
 names(pca_data)
 
-## * 8.2.18 Display five tweets in clusters from 18-----------------------------   :8218:
+## * 8.2.19 Display five tweets in clusters from 18-----------------------------   :8219:
 set.seed(314)
   for(i in 1:3) {
     n <- sample(which(pca_data$Cluster == i), size = 5)
@@ -640,4 +640,22 @@ set.seed(314)
     print("===========================")
   }
 
+## * 8.2.20 TODO REPORT What are the Important Themes --------------------------   :8220:
+## * 8.2.21 TODO Generate a word cloud of the clusters -------------------------   :8221:
+## We can't get the actual terms because word stemming removes the one-to-one
+## correspondence
 
+i <- 1
+n <- which(pca_data$Cluster == i)
+
+(relevant <- sort(apply(tweet_weighted_dtm[n,], 2, mean), decreasing = TRUE)[1:30]) %>% head()
+
+p <- brewer.pal(n = 5, name = "Set2")
+ wordcloud(
+   words = names(relevant),
+   freq = relevant,
+   colors = p,
+   random.color = FALSE
+ )
+## * 8.2.21 TODO Use a dendrogram to display the themes of the clusters --------   :8222:
+## * 8.2.22 TODO REPORT What is the Conclusion regarding Themes? ---------------   :8223: